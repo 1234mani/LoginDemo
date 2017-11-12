c61917327afc659b2d315ec00a5243293ff3060a
MANIKANTA M <sswami8888@gmail.com>
2017-11-12 10:56:13 +0530

    FB login Demo

---
+++ LoginDemo/LoginDemo/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  LoginDemo
+//
+//  Created by MANIKANTA M on 12/11/17.
+//  Copyright © 2017 MANIKANTA M. All rights reserved.
+//
+
+import UIKit
+import FacebookLogin
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        if let accessToken = AccessToken.current {
+//            // User is logged in, use 'accessToken' here.
+//        }
+        
+        let loginButton = LoginButton(readPermissions: [ .publicProfile,.email, .userFriends  ])
+        loginButton.center = view.center
+        
+        view.addSubview(loginButton)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
